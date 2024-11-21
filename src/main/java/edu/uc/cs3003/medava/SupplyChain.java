592ead29446ef4748aa6a30d221d7e1a5ab1e60a
clarehoop <clare.hooper02@gmail.com>
2024-11-21 15:15:41 -0500

    questions

--- src/main/java/edu/uc/cs3003/medava/SupplyChain.java
+++ src/main/java/edu/uc/cs3003/medava/SupplyChain.java
@@ -2,7 +2,8 @@ package edu.uc.cs3003.medava;
 
 public class SupplyChain {
     public static void main(String args[]) {
+        HospitalRunner.run();
        //System.out.println("Hello, <your name>! This is the WOPR speaking.");
-       HospitalRunner.run();
+       
     }
 }
